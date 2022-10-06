07a0185cd9038e7b6bd709f0a14afcba2b163267
cturlo <cturlo@nd.edu>
2022-10-06 18:14:54 -0400

    Re-wrote homework in new file

---
+++ exercise6.sh
@@ -0,0 +1,23 @@
+#Usage: cat file|bash exercise6.sh
+
+#Q1
+#Write a file containing unique gender/years experience
+#Replace the commas with a space
+#Sort rows by gender, then by years experience; keep pairings intact
+#Saves file as gender_and_experience.txt
+
+#Q2
+#Return following to stdout
+#Gender, years experience, wage (highest earner) (max)
+#Gender, years experience, wage (lowest earner) (min)
+#Number of females in top 10 earners
+
+#Q3
+#Return following to stdout
+#Effect of graduating college on minimum wage of dataset
+	#12 years v. 16 years of school
+
+#Assign output variable to min (12), and min (16)
+#Calculate difference from the values using bc
+
+

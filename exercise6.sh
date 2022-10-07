3e3222635e055dd5ed18ccb79a65a869d3e7aee2
cturlo <cturlo@nd.edu>
2022-10-07 02:15:20 -0400

    Finalized Exercise 6

--- exercise6.sh
+++ exercise6.sh
@@ -6,7 +6,7 @@
 
 
 cat $1|cut -d, -f 1,2|tail +2|sort -t, -k1,1 -k2,2n|sed 's/,/ /g'|uniq > gender_and_experience.txt
-#line below opens that file to verify, delete "# to check"
+#line below opens that file to verify, delete "#" to check
 #cat gender_and_experience.txt
 
 #Q2
@@ -19,14 +19,14 @@ cat $1|cut -d, -f 1,2|tail +2|sort -t, -k1,1 -k2,2n|sed 's/,/ /g'|uniq > gender_
 #isolate how many of top 10 earners are female
 echo "Number of females in Top 10 earners:"
 cat $1|sort -t, -k4rn|head -n +10|grep -a "female"|wc -l
-
 #isolate highest earner
 echo "Highest earner statistics"
-cat $1|sort -t, -k4rn|head -n +1
-
+cat $1|head -n +1| cut -d, -f 1,2,4
+cat $1|sort -t, -k4rn|head -n +1|cut -d, -f 1,2,4
 #isolate lowest earner
 echo "Lowest earner statistics"
-cat $1|sort -t, -k4n|head -n +2|tail -n -1
+cat $1|head -n +1| cut -d, -f 1,2,4
+cat $1|sort -t, -k4n|head -n +2|tail -n -1|cut -d, -f 1,2,4
 
 #Q3
 #Return following to stdout
@@ -35,7 +35,9 @@ cat $1|sort -t, -k4n|head -n +2|tail -n -1
 
 #Assign output variable to min (12), and min (16)
 #Calculate difference from the values using bc
-echo "Q3 Starts HERE"
 
-cat $1|sort -t, -k4rn|grep -E "12"
+echo "Difference between 16 years and 12 years minimum wage"
+min12=$(cat $1|cut -d, -f 3,4|grep -E "^12"|sort -t, -nk 2|cut -d, -f 2|head -n 1)
+min16=$(cat $1|cut -d, -f 3,4|grep -E "^16"|sort -t, -nk 2|cut -d, -f 2|head -n 1)
+echo "$min16 - $min12"|bc
 

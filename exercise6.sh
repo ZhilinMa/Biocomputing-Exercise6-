d0ccd28992a935fcbb3794d69a216664c1d27ac2
cturlo <cturlo@nd.edu>
2022-10-06 20:59:20 -0400

    Completed Q1

--- exercise6.sh
+++ exercise6.sh
@@ -1,19 +1,21 @@
 #Usage: cat file|bash exercise6.sh
 
 #Q1
-#Write a file containing unique gender/years experience
-#Replace the commas with a space
-#Sort rows by gender, then by years experience; keep pairings intact
-#Saves file as gender_and_experience.txt
+#Write a file containing unique gender/years experience (cut -d, -f 1,2|...|uniq > gender_and_experience.txt)
+#Replace the commas with a space (sed 's/,/ /g')
+#Sort rows by gender, then by years experience; keep pairings intact (sort -t, -k1,1 -k2,2n)
 
-cut -d, -f 1,2 > gender_and_experience.txt
-cat gender_and_experience.txt|tail +2|sort -t, -k2n
+
+cut -d, -f 1,2|tail +2|sort -t, -k1,1 -k2,2n|sed 's/,/ /g'|uniq > gender_and_experience.txt
+#opens that file
+cat gender_and_experience.txt
 
 #Q2
 #Return following to stdout
 #Gender, years experience, wage (highest earner) (max)
 #Gender, years experience, wage (lowest earner) (min)
 #Number of females in top 10 earners
+
 
 #Q3
 #Return following to stdout

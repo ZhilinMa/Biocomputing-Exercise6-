b24c3ac4e383e19ed226e436cd35f1c65093e3f7
ZhilinMa <91452609+ZhilinMa@users.noreply.github.com>
2022-10-24 16:42:04 -0400

    Update question1.sh

--- question1.sh
+++ question1.sh
@@ -1,1 +1,3 @@
 cat 'wages (1).csv' |cut -d , -f 1,2|sort -t, -n -k1| tr "," " "
+#you should get a unique gender-yearsExperience combinations here, so add -u to your sort
+#cat wages.csv | grep -v gender | cut -d , -f 1,2 | sort -u | tr ',' ' ' | sort -k1,1d -k2,2n >> categories.txt
